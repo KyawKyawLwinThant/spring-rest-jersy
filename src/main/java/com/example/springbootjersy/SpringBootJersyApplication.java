1a02d32ac33577c8bf52ea8ed67e0a81fe0d4900
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2019-07-27 16:13:07 +0630

    first commit

---
+++ src/main/java/com/example/springbootjersy/SpringBootJersyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjersy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJersyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootJersyApplication.class, args);
+  }
+
+}
